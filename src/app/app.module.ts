59d6f7015c4aae9f10350834c0c84e936e377c6d
nick <n7412440@gmail.com>
2019-08-22 17:52:18 +0300

    life cycle hook in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
+
 import { HttpClientModule } from '@angular/common/http';
-import { CounterComponent } from './counter/counter.component';
-import { ListComponent } from './list/list.component';
-import { TodosComponent } from './todos/todos.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
+import { AddCarComponent } from './add-car/add-car.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    CounterComponent,
-    ListComponent,
-    TodosComponent
+    CarsComponent,
+    CarComponent,
+    AddCarComponent,
   ],
   imports: [
     BrowserModule,
